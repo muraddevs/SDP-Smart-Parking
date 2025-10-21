bb771216581e3062ef878f56709542dd3c93100a
Murad Aghamirzayev <murad.aghamirzayevv@gmail.com>
2025-10-21 18:50:50 +0400

    Delete backendAPI directory

--- backendAPI/aiparkingsystem/parking/src/main/java/ai/parking/system/repository/ParkingSessionRepository.java
+++
@@ -1,8 +0,0 @@
-package ai.parking.system.repository;
-
-import ai.parking.system.entity.ParkingSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingSessionRepository extends JpaRepository<ParkingSession, Integer> {}
--- backendAPI/aiparkingsystem/parking/src/main/java/ai/parking/system/repository/ParkingSlotRepository.java
+++
@@ -1,8 +0,0 @@
-package ai.parking.system.repository;
-
-import ai.parking.system.entity.ParkingSlot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingSlotRepository extends JpaRepository<ParkingSlot, Integer> {}
